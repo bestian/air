658e0b7be5a5d595667bdc1440b2f7f6b1b5b67c
Bestian Tang 小巴 <bestian@gmail.com>
2022-10-13 15:32:53 +0800

    deploy

---
+++ js/about.477bf44c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkair"]=self["webpackChunkair"]||[]).push([[443],{5399:function(n,t,u){u.r(t),u.d(t,{default:function(){return l}});var s=function(){var n=this;n._self._c;return n._m(0)},a=[function(){var n=this,t=n._self._c;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],r=u(1001),e={},i=(0,r.Z)(e,s,a,!1,null,null,null),l=i.exports}}]);
+//# sourceMappingURL=about.477bf44c.js.map
